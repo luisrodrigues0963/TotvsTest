390d04fa9accf81129a1dc1ea2faad0cbf8df769
Luís Rodrigues <luis.rodrigues0963@gmail.com>
2021-04-27 11:05:39 +0100

    Criação do endpoint Change

---
+++ ChangeAPI/Controllers/ChangeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChangeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChangeController : ControllerBase
+    {
+        [HttpGet("{total}/{amountPaid}")]
+        public IActionResult Change(double total, double amountPaid)
+        {
+            return Ok();
+        }
+    }
+}
